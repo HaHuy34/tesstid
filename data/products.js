feeabb6d9992f17603384a089d42349486a50dd0
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2025-09-05 17:59:30 +0700

    update code

---
+++ data/products.js
@@ -0,0 +1,148 @@
+var list_products = [
+  {
+    name: "Đắp cao su rulo, trục, con lăn cao su đưa phôi",
+    company: "Băng tải",
+    img: "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/4(9).jpg",
+    price: "Liên hệ",
+    rateCount: 26,
+    promo: {
+      name: "bangtai",
+      value: "0",
+    },
+    detail: {
+      ten: "Đắp cao su rulo, trục, con lăn cao su đưa phôi",
+      model: "Android 8.1 (Oreo)",
+      tieuchuan: "13 MP",
+      xuatsu: "5 MP",
+      donggia: "Qualcomm Snapdragon 425 4 nhân 64-bit",
+      baohanh: "2 GB",
+      modelkhac: "16 GB",
+      detailcontent:
+        "Bánh đà là một thiết bị cơ khí quay được sử dụng để lưu trữ năng lượng quay. Bánh đà có mô men quán tính lớn, và do đó chống lại sự thay đổi tốc độ quay. Lượng năng lượng được lưu trữ trong một bánh đà tỉ lệ với bình phương tốc độ quay của nó. Năng lượng được chuyển giao cho một bánh đà bằng cách áp dụng mô men xoắn đối với nó, do đó gây ra tốc độ quay của nó, và do đó năng lượng lưu trữ của nó, gia tăng. Ngược lại, bánh đà giải phóng năng lượng được lưu trữ bằng cách áp mô-men xoắn đến tải cơ khí, kết quả làm tốc độ quay giảm.Chúng cung cấp năng lượng liên tục khi các nguồn năng lượng không liên tục. Ví dụ, bánh quay được sử dụng trong động cơ pitong bởi vì các nguồn năng lượng (mô-men xoắn từ động cơ) là không liên tục.Chúng cung cấp năng lượng ở mức vượt quá khả năng của một nguồn năng lượng. Điều này đạt được bằng cách thu thập năng lượng trong bánh đà theo thời gian và sau đó giải phóng năng lượng một cách nhanh chóng, với tốc độ vượt quá khả năng của nguồn năng lượng.Chúng kiểm soát định hướng của một hệ thống cơ khí. Trong các ứng dụng như vậy, xung lượng góc của một bánh đà là cố ý chuyển tải một khi năng lượng được chuyển đến hoặc từ bánh đà.",
+      iamge: {
+        iamge1:
+          "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/4(9).jpg",
+        iamge2:
+          "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/6(6).jpg",
+      },
+    },
+    masp: "Sam0",
+  },
+  {
+    name: "Băng tải cao su nhật Cũ và Mới các loại",
+    company: "Băng tải",
+    img: "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/1(11).jpg",
+    price: "Liên hệ",
+    rateCount: 26,
+    promo: {
+      name: "bangtai",
+      value: "0",
+    },
+    detail: {
+      ten: "Băng tải cao su nhật Cũ và Mới các loại",
+      modle: "Android 8.1 (Oreo)",
+      tieuchuan: "13 MP",
+      xuatsu: "5 MP",
+      donggia: "Qualcomm Snapdragon 425 4 nhân 64-bit",
+      baohanh: "2 GB",
+      modelkhac: "16 GB",
+      detailcontent:
+        "Bánh đà là một thiết bị cơ khí quay được sử dụng để lưu trữ năng lượng quay. Bánh đà có mô men quán tính lớn, và do đó chống lại sự thay đổi tốc độ quay. Lượng năng lượng được lưu trữ trong một bánh đà tỉ lệ với bình phương tốc độ quay của nó. Năng lượng được chuyển giao cho một bánh đà bằng cách áp dụng mô men xoắn đối với nó, do đó gây ra tốc độ quay của nó, và do đó năng lượng lưu trữ của nó, gia tăng. Ngược lại, bánh đà giải phóng năng lượng được lưu trữ bằng cách áp mô-men xoắn đến tải cơ khí, kết quả làm tốc độ quay giảm.Chúng cung cấp năng lượng liên tục khi các nguồn năng lượng không liên tục. Ví dụ, bánh quay được sử dụng trong động cơ pitong bởi vì các nguồn năng lượng (mô-men xoắn từ động cơ) là không liên tục.Chúng cung cấp năng lượng ở mức vượt quá khả năng của một nguồn năng lượng. Điều này đạt được bằng cách thu thập năng lượng trong bánh đà theo thời gian và sau đó giải phóng năng lượng một cách nhanh chóng, với tốc độ vượt quá khả năng của nguồn năng lượng.Chúng kiểm soát định hướng của một hệ thống cơ khí. Trong các ứng dụng như vậy, xung lượng góc của một bánh đà là cố ý chuyển tải một khi năng lượng được chuyển đến hoặc từ bánh đà.",
+      iamge: {
+        iamge1:
+          "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/1(11).jpg",
+        iamge2:
+          "https://maycongnghiep.net.vn/hinh-anh/images/tin%20tuc/1(11).jpg",
+      },
+    },
+    masp: "Sam0",
+  },
+  {
+    name: "Băng tải cao su chịu nhiệt",
+    company: "Băng tải",
+    img: "https://maycongnghiep.net.vn/hinh-anh/images/Bang%20Keo%20CN/b%E1%BB%8Dc%20rulo%20cao%20su.png",
+    price: "Liên hệ",
+    rateCount: 26,
+    promo: {
+      name: "bangtai",
+      value: "0",
+    },
+    detail: {
+      ten: "Băng tải cao su chịu nhiệt",
+      modle: "Android 8.1 (Oreo)",
+      tieuchuan: "13 MP",
+      xuatsu: "5 MP",
+      donggia: "Qualcomm Snapdragon 425 4 nhân 64-bit",
+      baohanh: "2 GB",
+      modelkhac: "16 GB",
+      detailcontent:
+        "Bánh đà là một thiết bị cơ khí quay được sử dụng để lưu trữ năng lượng quay. Bánh đà có mô men quán tính lớn, và do đó chống lại sự thay đổi tốc độ quay. Lượng năng lượng được lưu trữ trong một bánh đà tỉ lệ với bình phương tốc độ quay của nó. Năng lượng được chuyển giao cho một bánh đà bằng cách áp dụng mô men xoắn đối với nó, do đó gây ra tốc độ quay của nó, và do đó năng lượng lưu trữ của nó, gia tăng. Ngược lại, bánh đà giải phóng năng lượng được lưu trữ bằng cách áp mô-men xoắn đến tải cơ khí, kết quả làm tốc độ quay giảm.Chúng cung cấp năng lượng liên tục khi các nguồn năng lượng không liên tục. Ví dụ, bánh quay được sử dụng trong động cơ pitong bởi vì các nguồn năng lượng (mô-men xoắn từ động cơ) là không liên tục.Chúng cung cấp năng lượng ở mức vượt quá khả năng của một nguồn năng lượng. Điều này đạt được bằng cách thu thập năng lượng trong bánh đà theo thời gian và sau đó giải phóng năng lượng một cách nhanh chóng, với tốc độ vượt quá khả năng của nguồn năng lượng.Chúng kiểm soát định hướng của một hệ thống cơ khí. Trong các ứng dụng như vậy, xung lượng góc của một bánh đà là cố ý chuyển tải một khi năng lượng được chuyển đến hoặc từ bánh đà.",
+      iamge: {
+        iamge1:
+          "https://maycongnghiep.net.vn/hinh-anh/images/Bang%20Keo%20CN/b%E1%BB%8Dc%20rulo%20cao%20su.png",
+        iamge2:
+          "https://maycongnghiep.net.vn/hinh-anh/images/Bang%20Keo%20CN/b%E1%BB%8Dc%20rulo%20cao%20su.png",
+      },
+    },
+    masp: "Sam0",
+  },
+  {
+    name: "Rulo Bọc Cao Su,Conveyor, Nối Băng Tải, Dán Băng Tải, Bọc Cao Su Rulo",
+    company: "Băng tải",
+    img: "https://maycongnghiep.net.vn/hinh-anh/images/rulo-boc-cao-su.jpg",
+    price: "Liên hệ",
+    rateCount: 26,
+    promo: {
+      name: "bangtai",
+      value: "0",
+    },
+    detail: {
+      ten: "Rulo Bọc Cao Su,Conveyor, Nối Băng Tải, Dán Băng Tải, Bọc Cao Su Rulo",
+      modle: "Android 8.1 (Oreo)",
+      tieuchuan: "13 MP",
+      xuatsu: "5 MP",
+      donggia: "Qualcomm Snapdragon 425 4 nhân 64-bit",
+      baohanh: "2 GB",
+      modelkhac: "16 GB",
+      detailcontent:
+        "Bánh đà là một thiết bị cơ khí quay được sử dụng để lưu trữ năng lượng quay. Bánh đà có mô men quán tính lớn, và do đó chống lại sự thay đổi tốc độ quay. Lượng năng lượng được lưu trữ trong một bánh đà tỉ lệ với bình phương tốc độ quay của nó. Năng lượng được chuyển giao cho một bánh đà bằng cách áp dụng mô men xoắn đối với nó, do đó gây ra tốc độ quay của nó, và do đó năng lượng lưu trữ của nó, gia tăng. Ngược lại, bánh đà giải phóng năng lượng được lưu trữ bằng cách áp mô-men xoắn đến tải cơ khí, kết quả làm tốc độ quay giảm.Chúng cung cấp năng lượng liên tục khi các nguồn năng lượng không liên tục. Ví dụ, bánh quay được sử dụng trong động cơ pitong bởi vì các nguồn năng lượng (mô-men xoắn từ động cơ) là không liên tục.Chúng cung cấp năng lượng ở mức vượt quá khả năng của một nguồn năng lượng. Điều này đạt được bằng cách thu thập năng lượng trong bánh đà theo thời gian và sau đó giải phóng năng lượng một cách nhanh chóng, với tốc độ vượt quá khả năng của nguồn năng lượng.Chúng kiểm soát định hướng của một hệ thống cơ khí. Trong các ứng dụng như vậy, xung lượng góc của một bánh đà là cố ý chuyển tải một khi năng lượng được chuyển đến hoặc từ bánh đà.",
+      iamge: {
+        iamge1:
+          "https://maycongnghiep.net.vn/hinh-anh/images/rulo-boc-cao-su.jpg",
+        iamge2:
+          "https://maycongnghiep.net.vn/hinh-anh/images/rulo-boc-cao-su.jpg",
+      },
+    },
+    masp: "Sam0",
+  },
+  {
+    name: "Băng Tải Lưới,Conveyor, Nối Băng Tải, Dán Băng Tải, Bọc Cao Su Rulo",
+    company: "Băng tải",
+    img: "https://maycongnghiep.net.vn/hinh-anh/images/bang%20tai%20luoi%201.jpg",
+    price: "Liên hệ",
+    rateCount: 26,
+    promo: {
+      name: "bangtai",
+      value: "0",
+    },
+    detail: {
+      ten: "Đắp cao su rulo, trục, con lăn cao su đưa phôi",
+      modle: "Android 8.1 (Oreo)",
+      tieuchuan: "13 MP",
+      xuatsu: "5 MP",
+      donggia: "Qualcomm Snapdragon 425 4 nhân 64-bit",
+      baohanh: "2 GB",
+      modelkhac: "16 GB",
+      detailcontent:
+        "Bánh đà là một thiết bị cơ khí quay được sử dụng để lưu trữ năng lượng quay. Bánh đà có mô men quán tính lớn, và do đó chống lại sự thay đổi tốc độ quay. Lượng năng lượng được lưu trữ trong một bánh đà tỉ lệ với bình phương tốc độ quay của nó. Năng lượng được chuyển giao cho một bánh đà bằng cách áp dụng mô men xoắn đối với nó, do đó gây ra tốc độ quay của nó, và do đó năng lượng lưu trữ của nó, gia tăng. Ngược lại, bánh đà giải phóng năng lượng được lưu trữ bằng cách áp mô-men xoắn đến tải cơ khí, kết quả làm tốc độ quay giảm.Chúng cung cấp năng lượng liên tục khi các nguồn năng lượng không liên tục. Ví dụ, bánh quay được sử dụng trong động cơ pitong bởi vì các nguồn năng lượng (mô-men xoắn từ động cơ) là không liên tục.Chúng cung cấp năng lượng ở mức vượt quá khả năng của một nguồn năng lượng. Điều này đạt được bằng cách thu thập năng lượng trong bánh đà theo thời gian và sau đó giải phóng năng lượng một cách nhanh chóng, với tốc độ vượt quá khả năng của nguồn năng lượng.Chúng kiểm soát định hướng của một hệ thống cơ khí. Trong các ứng dụng như vậy, xung lượng góc của một bánh đà là cố ý chuyển tải một khi năng lượng được chuyển đến hoặc từ bánh đà.",
+      iamge: {
+        iamge1:
+          "https://maycongnghiep.net.vn/hinh-anh/images/bang%20tai%20luoi%201.jpg",
+        iamge2:
+          "https://maycongnghiep.net.vn/hinh-anh/images/bang%20tai%20luoi%201.jpg",
+      },
+    },
+    masp: "Sam0",
+  },
+  
+];
